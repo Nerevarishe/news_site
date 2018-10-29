0df43f223a3f6b51458d460d11f3852935e88787
Nerevarishe <JIyHaTuK@gmail.com>
2018-10-29 14:47:41 +0300

    working chat app

--- chat.py
+++ chat.py
@@ -18,4 +18,4 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8081, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5001, debug=True)
--- news_site.py
+++ news_site.py
@@ -10,12 +10,12 @@ def make_shell_context():
 
 # PyCharm runner:
 
-#if __name__ == '__main__':
-#    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
+if __name__ == '__main__':
+    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
 
 # c9.io runner
 
-if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), 
-            debug=True, use_debugger=False, use_reloader=False, 
-            passthrough_errors=True)
+#if __name__ == '__main__':
+#    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)),
+#            debug=True, use_debugger=False, use_reloader=False,
+#            passthrough_errors=True)
