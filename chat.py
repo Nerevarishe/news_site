b97c73189cddea9ff3cfd6d50ed64b17136026d3
Nerevarishe <JIyHaTuK@gmail.com>
2018-11-15 10:54:40 +0300

    chat: escaping html

--- chat.py
+++ chat.py
@@ -1,20 +1,28 @@
-from flask import Flask
+from flask import Flask, escape
 from flask_socketio import SocketIO
-from config import Config
 
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 
-def messageReceived(methods=['GET', 'POST']):
+def messageReceived():
     print('Message was received')
 
 
 @socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
+def handle_my_custom_event(json):
     print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+
+    # TODO: Убедиться в безопасности - всё ли экранирует функция
+    def escape_html(json):
+        response = {}
+        for k, v in json.items():
+            d = {k: escape(v)}
+            response.update(d)
+        return response
+
+    socketio.emit('my response', escape_html(json), callback=messageReceived)
 
 
 if __name__ == '__main__':
